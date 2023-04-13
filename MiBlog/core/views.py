2fb560fad5070e18ac6f7d98e0ebd4ef95b41f25
AgustinVaras <Agustin.Varas@outlook.com.ar>
2023-04-13 18:23:42 -0300

    Creacion de la app usuarios, armado de archivos .py necesarios e include en urls de MiBlog

--- MiBlog/core/views.py
+++ MiBlog/core/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, request
 
-#Importaciones para el login
-#----------------------------------------------------------------------------------------------------------------------------------------------
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import login, logout, authenticate
-
 #Importaciones para views basadas en clases
 #----------------------------------------------------------------------------------------------------------------------------------------------
 from django.views.generic import ListView
@@ -21,33 +16,6 @@ from .forms import PostAddForm, PostEditForm
 # Create your views here.
 def inicio(request):
     return render(request, 'core/home.html')
-    
-def login_form(request):
-
-    if request.method == "POST":
-        form = AuthenticationForm(request, data = request.POST)
-
-        if form.is_valid():
-            datos = form.cleaned_data()
-
-            usuario = datos['username']
-            passw = datos['password']
-
-            user = authenticate(username = usuario , password = passw)
-
-            if user is not None:
-                login(request, user)
-
-                return render(request, "core/home.html", {"mensaje":f"Bienvendio {usuario}"})
-            else:
-                return render(request, "core/login.html", {"mensaje":"Usuario o contraseña incorrecta", "form": form})
-        else:
-            return render(request, "core/home.html", {"mensaje": "Error en el formulario"})
-
-    else:
-        form = AuthenticationForm()
-
-    return render(request, "core/login.html", {"form":form})
 
 class PostsListarView(ListView):
     model = post 
---
+++ MiBlog/usuarios/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+#Importaciones para el login
+#----------------------------------------------------------------------------------------------------------------------------------------------
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+
+# Create your views here
+
