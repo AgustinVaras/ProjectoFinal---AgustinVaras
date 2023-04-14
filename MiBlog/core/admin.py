9c93028a654219e6c3656ad84e195cfeae296f2f
AgustinVaras <Agustin.Varas@outlook.com.ar>
2023-04-14 17:35:11 -0300

    registro de mi modelo de categorias en el admin.py

--- MiBlog/core/admin.py
+++ MiBlog/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from core.models import post
+from core.models import post, categoria
 
 
 # Register your models here.
 admin.site.register(post)
+admin.site.register(categoria)
