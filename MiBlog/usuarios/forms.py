92b7ed0b22140fe02f3d2054666457fde0cccb68
AgustinVaras <Agustin.Varas@outlook.com.ar>
2023-04-19 19:18:05 -0300

    Mas arreglo de Estilizado con bootsrap del formulario

--- MiBlog/usuarios/forms.py
+++ MiBlog/usuarios/forms.py
@@ -6,13 +6,13 @@ from django.contrib.auth.models import User
 
 
 class UsuarioRegistroForm(UserCreationForm):
-    username = forms.CharField(max_length=100, label='Usuario', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # username = forms.CharField(max_length=100, label='Usuario', widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(max_length=100,label='Nombre',widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(max_length=100, label='Apellido',widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label="Repetir contraseña", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    # password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    # password2 = forms.CharField(label="Repetir contraseña", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
@@ -20,3 +20,10 @@ class UsuarioRegistroForm(UserCreationForm):
 
         #mensajes de ayuda:
         help_texts = {k:"" for k in fields}
+    
+    def __init__(self, *args, **kwargs):
+        super(UsuarioRegistroForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
