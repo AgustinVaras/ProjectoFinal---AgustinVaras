3d36c2a73778262e7d7e50989a88c6f616662837
AgustinVaras <Agustin.Varas@outlook.com.ar>
2023-04-19 18:53:58 -0300

    Agregado de campos al form de registro y uso de bootsrap para estilizado

--- MiBlog/usuarios/forms.py
+++ MiBlog/usuarios/forms.py
@@ -6,14 +6,17 @@ from django.contrib.auth.models import User
 
 
 class UsuarioRegistroForm(UserCreationForm):
+    username = forms.CharField(max_length=100, label='Usuario', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100,label='Nombre',widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, label='Apellido',widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-    email = forms.EmailField()
-    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Repetir contraseña", widget=forms.PasswordInput)
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="Repetir contraseña", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
         #mensajes de ayuda:
         help_texts = {k:"" for k in fields}
