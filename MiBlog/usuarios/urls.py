283cb9a033f1956840ae29f20c6e6d791516bc66
AgustinVaras <Agustin.Varas@outlook.com.ar>
2023-04-19 20:57:45 -0300

    Creado del formulario y la view para edicion de perfil de usuario

--- MiBlog/usuarios/urls.py
+++ MiBlog/usuarios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UsuarioRegistroView
+from .views import UsuarioRegistroView, UsuarioEditView
 
 urlpatterns = [
     path('usuarios/', UsuarioRegistroView.as_view(), name='Registro'),
+    path('usuarios/editar/', UsuarioEditView.as_view(), name='Editar_Usuario'),
 ]
--- MiBlog/usuarios/views.py
+++ MiBlog/usuarios/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 #Importaciones para el login
 #----------------------------------------------------------------------------------------------------------------------------------------------
 from django.views import generic
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django.contrib.auth import login, logout, authenticate
 from django.urls import reverse_lazy
 
 #Importaciones de mis modelos y forms
 #----------------------------------------------------------------------------------------------------------------------------------------------
-from .forms import UsuarioRegistroForm
+from .forms import UsuarioRegistroForm 
 
 # Create your views here
 class UsuarioRegistroView(generic.CreateView):
@@ -17,3 +17,11 @@ class UsuarioRegistroView(generic.CreateView):
     template_name = 'registration/registro.html'
     success_url = reverse_lazy('login')
 
+
+class UsuarioEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/perfil_editar.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
