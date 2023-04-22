7b370c232597262df1f0fcc1407552a82d4c7da1
AgustinVaras <Agustin.Varas@outlook.com.ar>
2023-04-22 11:01:46 -0300

    Creacion del modelo de Perfil

--- MiBlog/usuarios/models.py
+++ MiBlog/usuarios/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from ckeditor.fields import RichTextField
+
 
 # Create your models here.
+
+class Perfil(models.Model):
+    usuario = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = RichTextField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.usuario)
+    
+    
